5fa7bcbaf91f5e683ea79affc54af5fc67d4bed7
Dinobi <architectstan01@gmail.com>
2023-03-07 02:34:50 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ for other class
 
 """
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     """BaseModel class to be inherited by other classes"""
@@ -15,16 +15,19 @@ class BaseModel:
             self.updated_at = datetime.now()
 
             if Kwargs is not None:
+                """ __class__ wont be added as an attribute """
                 if '__class__' in kwargs:
                     continue
     def __setattr__(self, name, value):
         # Name and Value attribute to be set here ...
     def __str__(self):
+        """Format `self` for output"""
         return "[{}] ({}) {}".format(self.__class__.name, self.id, self.__dict__)
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary containing all key/value pairs of __dict__"""
         dic = dict(self.__dict__)
         dic.update({'__class__': self.__class__.name
                 'created_at': isoformat()
