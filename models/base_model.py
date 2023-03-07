5b5aa682e19a27d28b80f299143d38e9ed63e648
Dinobi <architectstan01@gmail.com>
2023-03-06 23:35:16 -0800

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,19 @@ import datetime
 
 class BaseModel:
     """BaseModel class to be inherited by other classes"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self, id, created_at, updated_at):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.name, self.id, self.__dict__)
+    def save(self):
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        dic = dict(self.__dict__)
+        dic = {'__class_': self.__class__.name
+                'created_at': isoformat()
+                'updated_at': isoformat()
+                }
+        return dic
