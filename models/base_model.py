5355c89c5d3ee231ad37e098ca07149c784e79bb
Dinobi <architectstan01@gmail.com>
2023-03-06 20:10:28 -0800

    Base

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,15 @@
-#!/usr/bin/bash/python3
+#!/usr/bin/env python3
 """
 A class model that defines all common attributes/methods
 for other class
 
 """
-class BaseModel:
-#	def 
+import uuid
+import datetime
 
+class BaseModel:
+    """BaseModel class to be inherited by other classes"""
+    def __init__(self, *args, **kwargs):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
