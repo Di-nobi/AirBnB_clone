534a2298f167f88ab0c60d6aa4363a98716e9545
Dinobi <architectstan01@gmail.com>
2023-03-09 00:08:06 -0800

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@
 
  """
  import json
+ import models
 
  class FileStorage():
      __file_path = str(file.json)
@@ -12,8 +13,14 @@
 
      def all(self):
          return self.__objects
-     def new(self):
+     def new(self, obj):
          key = obj.__class__.__name__ + "." + obj.id
          self.__objects[key] = obj
 
+     def save(self):
 
+         with open('__file_path', 'w') as f:
+             dct = dict()
+             for name, obj in self.__objects.items():
+                 dct[name] = obj.to_dict()
+             json.dump(dct, f)
