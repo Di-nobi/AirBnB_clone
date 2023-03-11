0e4788af83ac4da4d1f0fefd7d4c6d9703e1ee26
KidusGR <kidmen10@gmail.com>
2023-03-11 13:25:34 +0300

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Import FileStorage and read existing data into `storage` variable"""
 
-from .engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
