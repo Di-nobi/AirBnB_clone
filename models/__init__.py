85ad2dab659f3af1cdef12d17fe95b0c9ad96277
KidusGR <kidmen10@gmail.com>
2023-03-11 13:41:24 +0300

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Saves objects in file to FileStorage class attribute __objects"""
-from models.engine.file_storage import FileStorage
+from .engine.file_storage import FileStorage
 
 
 storage = FileStorage()
